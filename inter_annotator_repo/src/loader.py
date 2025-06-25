34f6e659e9aa6c381ae5070dae71a55fb568b592
Aggeliki Linardatou <angela2003linardatou.com>
2025-06-25 22:08:37 +0300

    Change loader.py from class to function

--- inter_annotator_repo/src/loader.py
+++ inter_annotator_repo/src/loader.py
@@ -1,16 +1,13 @@
 import pandas as pd
 from pathlib import Path
 
-class AnnotationLoader:
-    """Load and prepare annotation Excel files."""
 
-    def __init__(self, annotation_dir: Path):
-        self.annotation_dir = annotation_dir
-
-    def load_files(self):
-        """Load Excel files from annotation directory."""
-        xlsx_files = list(self.annotation_dir.glob('*.xlsx'))
-        dataframes = {f.name: pd.read_excel(f, skiprows=1) for f in xlsx_files}
-        for fname, df in dataframes.items():
-            df.columns = ['id', 'text', 'anger', 'fear', 'joy', 'sadness', 'surprise']
-        return dataframes
+def load_annotation_files(
+    annotation_dir: Path
+) -> dict[str, pd.DataFrame]:
+    """Load Excel files from annotation directory."""
+    files = list(annotation_dir.glob("*.xlsx"))
+    return {
+        file.stem: pd.read_excel(file)
+        for file in files
+    }
