bd0fe00877eb0bd76f74adc26685818805746206
Aggeliki Linardatou <angela2003linardatou.com>
2025-06-26 18:38:24 +0300

    Put a line in data_loader.py

--- main_models_repo/src/data_loader.py
+++ main_models_repo/src/data_loader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pathlib import Path 
 
-def load_dataset(base_dir: Path, filename: str):
+def load_dataset(base_dir: Path, filename: str)-> pd.DataFrame:
     """Load Excel dataset from base directory."""
     file_path = self.base_dir / filename
     df = pd.read_csv(file_path)
