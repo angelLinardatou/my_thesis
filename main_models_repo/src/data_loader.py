698fe04974f2d573c626eddf761657af8f508eac
Aggeliki Linardatou <angela2003linardatou.com>
2025-06-26 19:15:04 +0300

    Delete self from the line in data_loader.py

--- main_models_repo/src/data_loader.py
+++ main_models_repo/src/data_loader.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 def load_dataset(base_dir: Path, filename: str)-> pd.DataFrame:
     """Load Excel dataset from base directory."""
-    file_path = self.base_dir / filename
+    file_path = base_dir / filename
     df = pd.read_csv(file_path)
     return df
  
