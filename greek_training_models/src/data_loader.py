2fe3c336619f6616711cdf6756d89d494a39bffa
Aggeliki Linardatou <angela2003linardatou.com>
2025-06-30 15:33:45 +0300

    Delele map_labels

--- greek_training_models/src/data_loader.py
+++ greek_training_models/src/data_loader.py
@@ -6,9 +6,4 @@ def load_dataset(base_dir: Path, filename: str)-> pd.DataFrame:
        file_path = base_dir / filename
        df = pd.read_csv(file_path)
        return df
-
-def map_labels(df: pd.DataFrame, mapping_column: str, label_column: str, mapping_dict: dict)-> pd.DataFrame:
-      """Map text labels to numerical format."""
-      df[label_column] = df[mapping_column].map(mapping_dict)
-      return df
  
